3ee0bbf297c2a471a0ea6e2fc443377ad572d1b7
olejorgen <oli_92jorgensen@hotmail.com>
2015-06-10 16:04:29 +0200

    Adding som methods not tested yet

--- coffe.py
+++ coffe.py
@@ -1,1 +1,12 @@
 __author__ = 'OleJorgen'
+from person import Person
+class Coffe(object):
+    fullname=" "
+    moeny=int
+    def __init__(self,fullname,money):
+        self.fullname=fullname
+        self.money=money
+    def CoffeBuying(self,money):
+        coffePrice=5
+        self.money=money-coffePrice
+        return money
